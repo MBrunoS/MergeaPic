2b0ea180bf56f10c2629ab78e7b39858125f6c88
Maurício Bruno <mb.bruno.s@gmail.com>
2023-03-20 22:52:00 -0300

    perf: adds goroutines, waitgroup and enhance error handling

--- main.go
+++ main.go
@@ -1,65 +1,83 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/draw"
 	"image/jpeg"
 	"image/png"
+	"io/fs"
 	"log"
 	"os"
-	"strconv"
+	"path/filepath"
+	"sync"
 
 	"github.com/disintegration/imaging"
 )
 
+var wg sync.WaitGroup
+
 func main() {
-	files, err := os.ReadDir("imagens")
+	overlayImgFile, err := os.Open("overlay.png")
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	// TARJA
-	overlayImgFile, err := os.Open("tarja.png")
-	if err != nil {
-		log.Fatalf("failed to open: %s", err)
-	}
-
-	overlayImg, err := png.Decode(overlayImgFile)
-	if err != nil {
-		log.Fatalf("failed to decode: %s", err)
+		log.Fatalf("failed to open overlay image: %s", err)
 	}
 	defer overlayImgFile.Close()
 
-	for i, file := range files {
-		photoFile, err := os.Open("imagens/" + file.Name())
-		if err != nil {
-			log.Fatalf("failed to open: %s", err)
-		}
-
-		bgImg, _, err := image.Decode(photoFile)
-		if err != nil {
-			log.Fatalf("failed to decode: %s", err)
-		}
-		defer photoFile.Close()
-
-		b := overlayImg.Bounds()
-		finalImg := image.NewRGBA(b)
-
-		resizedImg := imaging.Resize(bgImg, b.Size().X, 0, imaging.Lanczos)
-
-		offsetY := (resizedImg.Bounds().Dy() - b.Dy()) / 2
-
-		draw.Draw(finalImg, b, resizedImg, image.Point{0, offsetY}, draw.Src)
-		draw.Draw(finalImg, b, overlayImg, image.Point{}, draw.Over)
-
-		resultFileName := "tarjas/" + strconv.Itoa(i) + ".jpg"
-		result, err := os.Create(resultFileName)
-		if err != nil {
-			log.Fatalf("failed to create: %s", err)
-		}
-
-		jpeg.Encode(result, finalImg, &jpeg.Options{jpeg.DefaultQuality})
-		defer result.Close()
+	overlayImg, err := png.Decode(overlayImgFile)
+	if err != nil {
+		log.Fatalf("failed to decode overlay image: %s", err)
 	}
 
+	files, err := os.ReadDir("photos")
+	if err != nil {
+		log.Fatalf("Failed to read photos directory: %s", err)
+	}
+
+	for _, file := range files {
+		wg.Add(1)
+		go mergeImages(file, overlayImg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("All images merged successfully.")
+}
+
+func mergeImages(photoFile fs.DirEntry, overlayImg image.Image) {
+	defer wg.Done()
+
+	photoReader, err := os.Open(filepath.Join("photos", photoFile.Name()))
+	if err != nil {
+		log.Fatalf("failed to open photo %s: %s", photoFile.Name(), err)
+	}
+	defer photoReader.Close()
+
+	bgImg, _, err := image.Decode(photoReader)
+	if err != nil {
+		log.Fatalf("failed to decode photo %s: %s", photoFile.Name(), err)
+	}
+
+	b := overlayImg.Bounds()
+	finalImg := image.NewRGBA(b)
+
+	resizedImg := imaging.Fill(bgImg, b.Size().X, b.Size().Y, imaging.Center, imaging.Lanczos)
+
+	draw.Draw(finalImg, b, resizedImg, image.Point{}, draw.Src)
+	draw.Draw(finalImg, b, overlayImg, image.Point{}, draw.Over)
+
+	resultFileName := filepath.Join("merged_images", fmt.Sprintf("%s_merged.jpg", photoFile.Name()))
+	if err := os.MkdirAll(filepath.Dir(resultFileName), 0755); err != nil {
+		log.Fatalf("Failed to create merged images directory: %s", err)
+	}
+
+	result, err := os.Create(resultFileName)
+	if err != nil {
+		log.Fatalf("failed to create merged image file %s: %s", resultFileName, err)
+	}
+	defer result.Close()
+
+	if err := jpeg.Encode(result, finalImg, &jpeg.Options{Quality: jpeg.DefaultQuality}); err != nil {
+		log.Fatalf("Failed to encode merged image file %s: %s", resultFileName, err)
+	}
 }
