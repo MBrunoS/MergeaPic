5814ed621ae6c4c6f5d207ef4bfe08772ffff215
Maurício Bruno <mb.bruno.s@gmail.com>
2023-03-22 14:40:42 -0300

    feat: improve layout of preview list

--- frontend/src/components/PreviewList.tsx
+++ frontend/src/components/PreviewList.tsx
@@ -1,4 +1,5 @@
-import { Image } from "@chakra-ui/react";
+import { Flex, IconButton, Image, Stack, Text } from "@chakra-ui/react";
+import { X } from "@phosphor-icons/react";
 import React, { useContext, useEffect } from "react";
 import { Preview } from "../@types";
 import { AppContext } from "../context/AppContext";
@@ -18,11 +19,40 @@ export const PreviewList: React.FC = () => {
     };
   }, [images]);
 
+  function handleRemove(name: string) {
+    const newPreviews = previews.filter((preview) => preview.alt !== name);
+    setPreviews(newPreviews);
+  }
+
   return (
-    <>
+    <Stack maxW="4xl">
       {previews.map((item) => (
-        <Image src={item.src} alt={item.alt} key={item.alt} />
+        <Flex
+          key={item.alt}
+          align="center"
+          gap={4}
+          justify="space-between"
+          rounded="md"
+          backgroundColor="white"
+          _dark={{ backgroundColor: "gray.800" }}
+          overflow="hidden"
+        >
+          <Image
+            src={item.src}
+            alt={item.alt}
+            w={{ base: "35%", md: "30%", lg: "20%" }}
+          />
+          <Text>{item.alt}</Text>
+          <IconButton
+            colorScheme="red"
+            aria-label="Remove"
+            variant="ghost"
+            icon={<X />}
+            onClick={() => handleRemove(item.alt)}
+            mr={4}
+          />
+        </Flex>
       ))}
-    </>
+    </Stack>
   );
 };
