2e7312321a7424ad8d90ec3daa4e1ab9bcbb8614
Maurício Bruno <mb.bruno.s@gmail.com>
2023-04-10 13:48:48 -0300

    feat(backend): replace server to use nodejs sharp lib

---
+++ backend/src/merge-images.ts
@@ -0,0 +1,20 @@
+import sharp from "sharp";
+
+export async function mergeImages(
+  overlay: Express.Multer.File,
+  photos: Express.Multer.File[]
+) {
+  const overlayImage = sharp(overlay.buffer);
+  const { width, height } = await overlayImage.metadata();
+
+  return Promise.all(
+    photos.map((photo) =>
+      sharp(photo.buffer)
+        .resize(width, height)
+        .composite([{ input: overlay.buffer }])
+        .toFormat("jpg")
+        .toBuffer()
+        .then((data) => data.toString("base64"))
+    )
+  );
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { mergeImages } from "./merge-images.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const upload = multer();
+
+interface MulterRequest extends Express.Request {
+  files?: any;
+}
+
+const middleware = upload.fields([
+  {
+    name: "overlay",
+    maxCount: 1,
+  },
+  {
+    name: "photos[]",
+  },
+]);
+
+app.post("/merge", middleware, async (req: MulterRequest, res) => {
+  const overlay = req.files["overlay"][0];
+  const photos = req.files["photos[]"];
+
+  const images = await mergeImages(overlay, photos);
+
+  return res.json({ images });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
