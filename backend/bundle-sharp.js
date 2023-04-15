4073336e8df00679446202ab3e3dcf040534da0d
Maurício Bruno <mb.bruno.s@gmail.com>
2023-04-15 12:16:43 -0300

    build(backend): fix import errors

--- backend/bundle-sharp.js
+++ backend/bundle-sharp.js
@@ -1,41 +1,33 @@
-import { cp } from "fs/promises";
+const cp = require("fs").promises.cp;
 
 const copySharp = async () => {
-  await cp("./node_modules/sharp", "./build/node_modules/sharp", {
+  cp("./node_modules/sharp", "./build/node_modules/sharp", {
     recursive: true,
   });
-  await cp("./node_modules/color", "./build/node_modules/color", {
+  cp("./node_modules/color", "./build/node_modules/color", {
     recursive: true,
   });
-  await cp(
-    "./node_modules/color-convert",
-    "./build/node_modules/color-convert",
-    {
-      recursive: true,
-    }
-  );
-  await cp("./node_modules/color-name", "./build/node_modules/color-name", {
+  cp("./node_modules/color-convert", "./build/node_modules/color-convert", {
     recursive: true,
   });
-  await cp("./node_modules/color-string", "./build/node_modules/color-string", {
+  cp("./node_modules/color-name", "./build/node_modules/color-name", {
     recursive: true,
   });
-  await cp("./node_modules/detect-libc", "./build/node_modules/detect-libc", {
+  cp("./node_modules/color-string", "./build/node_modules/color-string", {
     recursive: true,
   });
-  await cp("./node_modules/is-arrayish", "./build/node_modules/is-arrayish", {
+  cp("./node_modules/detect-libc", "./build/node_modules/detect-libc", {
     recursive: true,
   });
-  await cp("./node_modules/semver", "./build/node_modules/semver", {
+  cp("./node_modules/is-arrayish", "./build/node_modules/is-arrayish", {
     recursive: true,
   });
-  await cp(
-    "./node_modules/simple-swizzle",
-    "./build/node_modules/simple-swizzle",
-    {
-      recursive: true,
-    }
-  );
+  cp("./node_modules/semver", "./build/node_modules/semver", {
+    recursive: true,
+  });
+  cp("./node_modules/simple-swizzle", "./build/node_modules/simple-swizzle", {
+    recursive: true,
+  });
 };
 
 copySharp();
